0b1efe5639e911cd099326fdbe2deedb135fce18
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-16 00:16:47 -0300

    feat: :sparkles: Adicionado README.md detalhado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP Server running on https://localhost:3333')
-})
+app.get('/hello', () => {
+  return 'Hello World!';
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀🚀 HTTP Server running on https://localhost:3333');
+  });

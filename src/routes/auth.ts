967270f8893898029e0206fdea4cd3db06f9c8c3
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-20 16:11:25 -0300

    fix: :sneake: Ajustando prettier eslint rocketseat

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,15 @@
-import axios from 'axios'
-import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
+import axios from 'axios';
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/register', async (request) => {
     const bodySchema = z.object({
       code: z.string(),
-    })
+    });
 
-    const { code } = bodySchema.parse(request.body)
+    const { code } = bodySchema.parse(request.body);
 
     const accessTokenResponse = await axios.post(
       'https://github.com/login/oauth/access_token',
@@ -23,31 +23,31 @@ export async function authRoutes(app: FastifyInstance) {
         headers: {
           Accept: 'application/json',
         },
-      },
-    )
+      }
+    );
 
-    const { access_token } = accessTokenResponse.data
+    const { access_token } = accessTokenResponse.data;
 
     const userResponse = await axios.get('https://api.github.com/user', {
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
-    })
+    });
 
     const userSchema = z.object({
       id: z.number(),
       login: z.string(),
       name: z.string(),
       avatar_url: z.string().url(),
-    })
+    });
 
-    const userInfo = userSchema.parse(userResponse.data)
+    const userInfo = userSchema.parse(userResponse.data);
 
     let user = await prisma.user.findUnique({
       where: {
         githubId: userInfo.id,
       },
-    })
+    });
 
     if (!user) {
       user = await prisma.user.create({
@@ -57,9 +57,9 @@ export async function authRoutes(app: FastifyInstance) {
           name: userInfo.name,
           avatarUrl: userInfo.avatar_url,
         },
-      })
+      });
     }
 
-    return { user }
-  })
+    return { user };
+  });
 }

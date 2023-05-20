1864b94c4e86856b4750761cc676a2058febfea0
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-20 15:41:32 -0300

    feat: :sparkles: Adicionado roteamento para auth

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import cors from '@fastify/cors';
+import 'dotenv/config';
 import fastify from 'fastify';
+import { authRoutes } from './routes/auth';
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
 
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app.register(cors, {
   origin: true,
